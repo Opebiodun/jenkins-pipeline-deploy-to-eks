568fc7f961e99d1de22a3822514a623acf1b37a6
Opebiodun <opebiodun@gmail.com>
2024-03-01 20:44:58 +0100

    updated

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
-#terraform {
-#  backend "s3" {
-#    bucket = "caps-app"
-#    region = "us-east-1"
-#    key = "eks/terraform.tfstate"
-#  }
-#}
+terraform {
+  backend "s3" {
+    bucket = "caps-app"
+   region = "us-east-1"
+   key = "eks/terraform.tfstate"
+  }
+}
