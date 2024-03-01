ac87faa5e89341fcab75296eafd83eb7689d57b3
Opebiodun <opebiodun@gmail.com>
2024-03-01 22:16:46 +0100

    updated

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,30 +1,25 @@
 module "eks" {
-    source  = "terraform-aws-modules/eks/aws"
-    version = "~> 20.0"
+  source  = "terraform-aws-modules/eks/aws"
 
-    cluster_name = "myapp-eks-cluster"
-    cluster_version = "1.29"
+  cluster_name                  = "myapp-eks-cluster"
+  cluster_version               = "1.21"
+  cluster_endpoint_public_access = true
 
-    cluster_endpoint_public_access  = true
+  vpc_id      = module.myapp-vpc.vpc_id
+  subnet_ids  = module.myapp-vpc.private_subnets
 
-    
+  tags = {
+    environment = "development"
+  }
 
-    vpc_id = module.myapp-vpc.vpc_id
-    subnet_ids = module.myapp-vpc.private_subnets
-
-    tags = {
-        environment = "development"
-        application = "myapp
-        }
-
-    eks_managed_node_groups = {
-        dev = {
-             min_size     = 1
-             max_size     = 3
-             desired_size = 2
-            
-            instance_types = ["t2.small"]
-            capacity_type  = "SPOT"
-            }
-        }
+  eks_managed_node_groups = {
+    dev = {
+      min_size        = 1
+      max_size        = 3
+      desired_size    = 2
+      instance_types  = ["t2.small"]
+      key_name        = "jenkins-server"
+      application     = "myapp"
     }
+  }
+}
