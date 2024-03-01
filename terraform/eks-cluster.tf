23c55f0504a61abc3fc606efa6d1150a4a717800
Opebiodun <opebiodun@gmail.com>
2024-03-01 10:50:18 +0100

    updated

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,10 +1,22 @@
 module "eks" {
-    source  = "terraform-aws-modules/eks/aws"
-    version = "~> 19.0"
+    source  = "terraform-aws-modules/eks/aws/latest" 
+    version = "~> 20.0"
     cluster_name = "myapp-eks-cluster"
-    cluster_version = "1.24"
+    cluster_version = "1.29"
 
     cluster_endpoint_public_access  = true
+
+    cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
 
     vpc_id = module.myapp-vpc.vpc_id
     subnet_ids = module.myapp-vpc.private_subnets
@@ -12,14 +24,15 @@ module "eks" {
     tags = {
         environment = "development"
         application = "myapp"
+        Terraform   = "true"
     }
 
     eks_managed_node_groups = {
         dev = {
-            min_size = 1
-            max_size = 3
+            min_size     = 1
+            max_size     = 3
             desired_size = 2
-
+            
             instance_types = ["t2.small"]
         }
     }
