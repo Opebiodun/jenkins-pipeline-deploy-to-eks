e17e58988d619817d97e4fe05c42372e0f381723
Opebiodun <opebiodun@gmail.com>
2024-03-01 11:35:32 +0100

    updated

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,5 +1,5 @@
 module "eks" {
-    source  = "terraform-aws-modules/eks/aws/latest" 
+    source  = "terraform-aws-modules/eks/aws" 
     version = "~> 20.0"
     cluster_name = "myapp-eks-cluster"
     cluster_version = "1.29"
